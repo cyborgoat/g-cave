4c14e1affab3b06d14da7faf355ea5ccb2d1e0cf
cyborgoat <cyborgoat@outlook.com>
2023-09-17 09:27:57 +0800

    updated: styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,14 +3,17 @@ import React from "react";
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/react";
 import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button, RadioGroup, Radio} from "@nextui-org/react";
 import {AcmeLogo} from "./AcmeLogo";
+import process from "process";
 
 
 export default function App() {
+    const rootPrefix = process.env.NEXT_PUBLIC_ROOT_PREFIX
+    const webPrefix = rootPrefix ? location.pathname : ""
     return (
         <Navbar shouldHideOnScroll className="h-10 mt-4 bg-transparent w-full mx-auto">
             <NavbarBrand>
                 <NavbarItem className="hidden lg:flex">
-                    <a className="font-normal" href={'/'}>
+                    <a className="font-normal" href={webPrefix}>
                         <span>Junxiao </span>
                         <span className="underline underline-offset-4
                         transition duration-300 ease-in-out hover:decoration-amber-500

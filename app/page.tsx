7246e8372f3c5ebb526c755b633318d38522a49e
cyborgoat <cyborgoat@outlook.com>
2023-09-17 15:46:09 +0800

    updated: styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import useSWR from "swr";
 import React from "react";
 import LoadingCircles from "@components/LoadingCircles";
 
-import {ScrollShadow} from "@nextui-org/react";
+import {Chip, ScrollShadow} from "@nextui-org/react";
 import {BlogInfo} from "../types/blog";
-import {DEV_MODE} from "./settings";
 import * as process from "process";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
@@ -22,30 +21,37 @@ export default function Home() {
     const webPrefix = rootPrefix ? location.pathname : ""
     return (
         <div className="grid gap-x-16 gap-y-4 grid-cols-2 w-full">
-            <div className="text-5xl bg-slate-800/30">
-                Blog <br/> Posts
-            </div>
-            <div className="text-5xl bg-indigo-800/30">
-                Music <br/> Productions
-            </div>
-            <ScrollShadow className="w-full h-4/5">
-                <ul className="list-disc list-inside">
-                    {blogList.map((blogInfo, ix) => {
-                        return (
-                            <li key={ix} className="mt-2">
-                                <a href={`${webPrefix}/blog/detail?category=${blogInfo.category}&title=${blogInfo.fname.replace('.json', '')}`}
-                                   className="hover:text-sky-400 duration-300"
-                                >
-                                    {blogInfo.title}
-                                </a>
-                            </li>
-                        )
+            <div className="group h-3/5">
+                <div className="bg-slate-800/30 falling-head">
+                    Blog <br/> Posts
+                </div>
+                <ScrollShadow className="w-full h-4/5 under-head">
+                    <ul className="list-disc list-inside">
+                        {blogList.map((blogInfo, ix) => {
+                            return (
+                                <li key={ix} className="mt-2">
+                                    <a href={`${webPrefix}/blog/detail?category=${blogInfo.category}&title=${blogInfo.fname.replace('.json', '')}`}
+                                       className="hover:text-zinc-900 duration-300 group/item"
+                                    >
+                                        {blogInfo.title}
+                                        <div className="ml-2 transition-opacity ease-in-out delay-150 opacity-0 inline-block group-hover/item:opacity-100">
+                                            <Chip size="sm">{blogInfo.category}</Chip>
+                                        </div>
+                                    </a>
+                                </li>
+                            )
 
-                    })}
-                </ul>
-            </ScrollShadow>
-            <div className="w-1/2 col-span-1">
-                Section under construction...
+                        })}
+                    </ul>
+                </ScrollShadow>
+            </div>
+            <div className="group h-3/5">
+                <div className="bg-indigo-800/30  falling-head">
+                    Music <br/> Productions
+                </div>
+                <div className="w-1/2 col-span-1 under-head">
+                    Section under construction...
+                </div>
             </div>
         </div>
     )

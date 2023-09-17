fb33f74563aa43a3e99b99f7138bd0bb9a04f071
cyborgoat <cyborgoat@outlook.com>
2023-09-17 09:42:46 +0800

    updated: styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ import process from "process";
 
 export default function App() {
     const rootPrefix = process.env.NEXT_PUBLIC_ROOT_PREFIX
-    const webPrefix =  "/"
+    const webPrefix = rootPrefix ? rootPrefix : "/"
     return (
         <Navbar shouldHideOnScroll className="h-10 mt-4 bg-transparent w-full mx-auto">
             <NavbarBrand>

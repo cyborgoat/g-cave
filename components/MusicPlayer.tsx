aebd3eb476c511782cf731fd862383e84cdd957f
cyborgoat <cyborgoat@outlook.com>
2023-10-22 19:51:06 +0800

    feat: music player

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -6,12 +6,19 @@ import PreviousIcon from "@components/icon/PreviousIcon";
 import PlayAndPauseIcon from "@components/icon/PlayAndPauseIcon";
 import NextSongIcon from "@components/icon/NextSongIcon";
 import RepeatIcon from "@components/icon/RepeatIcon";
+import useSound from "use-sound";
+import {MusicInfo} from "../types/music";
 
 export interface MusicPlayerProps extends CardProps {
 }
 
 export const MusicPlayer: FC<MusicPlayerProps> = ({className, ...otherProps}) => {
     const [liked, setLiked] = useState(false);
+    let music: MusicInfo = {
+        title: 'Night Owl',
+        musicUrl: '/assets/music/NightOwl.wav',
+        albumUrl: '/assets/music/NightOwl.jpeg',
+    }
 
     return (
         <Card
@@ -28,7 +35,7 @@ export const MusicPlayer: FC<MusicPlayerProps> = ({className, ...otherProps}) =>
                             className="object-cover"
                             height={200}
                             shadow="lg"
-                            src="/images/album-cover.png"
+                            src={music.albumUrl}
                             width="100%"
                         />
                     </div>
@@ -38,7 +45,7 @@ export const MusicPlayer: FC<MusicPlayerProps> = ({className, ...otherProps}) =>
                             <div className="flex flex-col gap-0">
                                 <h3 className="font-semibold text-foreground/90">Daily Mix</h3>
                                 <p className="text-sm text-foreground/80">0 Tracks</p>
-                                <h1 className="text-lg font-medium mt-2">Content Coming Soon...</h1>
+                                <h1 className="text-lg font-medium mt-2">{music.title}</h1>
                             </div>
                             <Button
                                 isIconOnly
@@ -50,58 +57,64 @@ export const MusicPlayer: FC<MusicPlayerProps> = ({className, ...otherProps}) =>
                                 <HeartIcon/>
                             </Button>
                         </div>
+                        <audio
+                            controls
+                            src={music.musicUrl}>
+                            Your browser does not support the
+                            <code>audio</code> element.
+                        </audio>
 
-                        <div className="flex flex-col mt-3 gap-1">
-                            <Progress
-                                aria-label="Music progress"
-                                classNames={{
-                                    indicator: "bg-default-800 dark:bg-white",
-                                    track: "bg-default-500/30",
-                                }}
-                                color="default"
-                                size="sm"
-                                value={33}
-                            />
-                            <div className="flex justify-between">
-                                <p className="text-sm">1:23</p>
-                                <p className="text-sm text-foreground/50">4:32</p>
-                            </div>
-                        </div>
+                        {/*<div className="flex flex-col mt-3 gap-1">*/}
+                        {/*    <Progress*/}
+                        {/*        aria-label="Music progress"*/}
+                        {/*        classNames={{*/}
+                        {/*            indicator: "bg-default-800 dark:bg-white",*/}
+                        {/*            track: "bg-default-500/30",*/}
+                        {/*        }}*/}
+                        {/*        color="default"*/}
+                        {/*        size="sm"*/}
+                        {/*        value={33}*/}
+                        {/*    />*/}
+                        {/*    <div className="flex justify-between">*/}
+                        {/*        <p className="text-sm">1:23</p>*/}
+                        {/*        <p className="text-sm text-foreground/50">4:32</p>*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
 
-                        <div className="flex w-full items-center justify-center">
-                            <Button
-                                isIconOnly
-                                className="data-[hover]:bg-foreground/10"
-                                radius="full"
-                                variant="light"
-                            >
-                                <RepeatIcon/>
-                            </Button>
-                            <Button
-                                isIconOnly
-                                className="data-[hover]:bg-foreground/10"
-                                radius="full"
-                                variant="light"
-                            >
-                                <PreviousIcon/>
-                            </Button>
-                            <Button
-                                isIconOnly
-                                className="w-auto h-auto data-[hover]:bg-foreground/10"
-                                radius="full"
-                                variant="light"
-                            >
-                                <PlayAndPauseIcon/>
-                            </Button>
-                            <Button
-                                isIconOnly
-                                className="data-[hover]:bg-foreground/10"
-                                radius="full"
-                                variant="light"
-                            >
-                                <NextSongIcon/>
-                            </Button>
-                        </div>
+                        {/*<div className="flex w-full items-center justify-center">*/}
+                        {/*    <Button*/}
+                        {/*        isIconOnly*/}
+                        {/*        className="data-[hover]:bg-foreground/10"*/}
+                        {/*        radius="full"*/}
+                        {/*        variant="light"*/}
+                        {/*    >*/}
+                        {/*        <RepeatIcon/>*/}
+                        {/*    </Button>*/}
+                        {/*    <Button*/}
+                        {/*        isIconOnly*/}
+                        {/*        className="data-[hover]:bg-foreground/10"*/}
+                        {/*        radius="full"*/}
+                        {/*        variant="light"*/}
+                        {/*    >*/}
+                        {/*        <PreviousIcon/>*/}
+                        {/*    </Button>*/}
+                        {/*    <Button*/}
+                        {/*        isIconOnly*/}
+                        {/*        className="w-auto h-auto data-[hover]:bg-foreground/10"*/}
+                        {/*        radius="full"*/}
+                        {/*        variant="light"*/}
+                        {/*    >*/}
+                        {/*        <PlayAndPauseIcon/>*/}
+                        {/*    </Button>*/}
+                        {/*    <Button*/}
+                        {/*        isIconOnly*/}
+                        {/*        className="data-[hover]:bg-foreground/10"*/}
+                        {/*        radius="full"*/}
+                        {/*        variant="light"*/}
+                        {/*    >*/}
+                        {/*        <NextSongIcon/>*/}
+                        {/*    </Button>*/}
+                        {/*</div>*/}
                     </div>
                 </div>
             </CardBody>
---
+++ types/music.d.ts
@@ -0,0 +1,5 @@
+export interface MusicInfo {
+    title: string,
+    musicUrl: string,
+    albumUrl: string
+}

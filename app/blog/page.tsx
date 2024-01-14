f4b81c3d14fdcc52d94dfe60afb0dcc76f5b1620
cyborgoat <cyborgoat@outlook.com>
2024-01-14 10:47:22 +0800

    updated: blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@ import useSWR from "swr";
 import React from "react";
 import LoadingCircles from "@components/LoadingCircles";
 import { BlogInfo } from "../../types/blog";
-import { ScrollShadow } from "@nextui-org/react";
+import { Card, Skeleton } from "@nextui-org/react";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const BLOG_URL_PREFIX =
@@ -16,7 +16,25 @@ export default function BlogHome() {
 
   if (error) return "An error has occurred.";
 
-  if (isLoading) return <LoadingCircles />;
+  if (isLoading)
+    return (
+      <Card className="w-3/5 mx-auto space-y-5 p-4" radius="lg">
+        <Skeleton className="rounded-lg">
+          <div className="h-24 rounded-lg bg-default-300"></div>
+        </Skeleton>
+        <div className="space-y-3">
+          <Skeleton className="w-3/5 rounded-lg">
+            <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
+          </Skeleton>
+          <Skeleton className="w-4/5 rounded-lg">
+            <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
+          </Skeleton>
+          <Skeleton className="w-2/5 rounded-lg">
+            <div className="h-3 w-2/5 rounded-lg bg-default-300"></div>
+          </Skeleton>
+        </div>
+      </Card>
+    );
 
   let blogList = data as BlogInfo[];
   return (
@@ -36,7 +54,9 @@ export default function BlogHome() {
             <div className="text-regular font-normal text-zinc-500 dark:text-zinc-300">
               {blogInfo.summary}
             </div>
-            <div className="pt-6 text-sm text-zinc-400/80">Posted on {blogInfo.date}</div>
+            <div className="pt-6 text-sm text-zinc-400/80">
+              Posted on {blogInfo.date}
+            </div>
           </a>
         );
       })}
